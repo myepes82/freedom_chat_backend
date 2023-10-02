439213b0fdc7868c5eb0d4cd40ade7a8a37a170a
Marlon Yepes <myepes82@misena.edu.co>
2023-10-02 13:09:44 -0500

    Users serrver divition

--- services/users/src/server.ts
+++ services/users/src/server.ts
@@ -1,7 +1,17 @@
-import express, { Express } from "express"
+import express, { Express, json, urlencoded } from "express";
+import cors from "cors";
+import { parseSuccessResponse } from "./utils/responseParser.utils";
+import userRestAdapter from "./adapters/in/userRest.adapter";
 
 const app = express();
 
-app.get("/", (_, res) => {})
+app.use(json({limit: "10mb"}));
+app.use(urlencoded({extended: true}))
+app.use(cors({methods: "*", origin: "*"}))
+ 
+
+app.use("/api/users", userRestAdapter)
+app.get("/", (_, res) => parseSuccessResponse<any>(res, { status: "ok" }, 200));
+
 
 export default app as Express;
